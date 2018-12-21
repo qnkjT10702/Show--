9552bff694aacf36ccb8ed68471d39c7c7796e57
Fananly <1835676864@qq.com>
2018-12-21 11:34:31 +0800

    1211

---
+++ Enterprisepersonalitydisplay/DataSheet.Model/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSheet.Model
+{
+    public class UserInfo
+    {
+        //用户信息表
+        public int UserId { get; set; }     //用户id
+        public string UserName { get; set; }  //用户名
+        public string UserPwd { get; set;}  //用户密码
+
+    }
+}
