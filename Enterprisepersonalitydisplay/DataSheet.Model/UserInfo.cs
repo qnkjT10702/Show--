e89ce9fb0c22d6773284f6e93e0dbdff466c21d5
Canlian123 <45959433+Canlian123@users.noreply.github.com>
2018-12-21 12:19:37 +0800

    111

--- Enterprisepersonalitydisplay/DataSheet.Model/UserInfo.cs
+++ Enterprisepersonalitydisplay/DataSheet.Model/UserInfo.cs
@@ -12,6 +12,9 @@ namespace DataSheet.Model
         public int UserId { get; set; }     //用户id
         public string UserName { get; set; }  //用户名
         public string UserPwd { get; set;}  //用户密码
-
+        public int  UserAge { get; set;} //用户年龄
+        public string UserSex { get; set; } //性别
+        public string UserEmall { get; set; } //qq邮箱
+        public string HeadImg { get; set; } //头像
     }
 }
