f59f8cd925b6ca6da59654856aa91ac946cf536a
LiaoxinJion <45959420+LiaoxinJion@users.noreply.github.com>
2018-12-24 09:11:38 +0800

    首页头部完成

--- Enterprisepersonalitydisplay/Enterprise.UI/Site1.Master.designer.cs
+++ Enterprisepersonalitydisplay/Enterprise.UI/Site1.Master.designer.cs
@@ -40,6 +40,15 @@ namespace Enterprise.UI {
         protected global::System.Web.UI.WebControls.TextBox searchBox;
         
         /// <summary>
+        /// searchBut 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button searchBut;
+        
+        /// <summary>
         /// ContentPlaceHolder1 控件。
         /// </summary>
         /// <remarks>
