f216ffdea1617d429cf1729f07abb16a652476f5
Canlian123 <45959433+Canlian123@users.noreply.github.com>
2018-12-24 09:12:39 +0800

    111

---
+++ Enterprisepersonalitydisplay/DataSheet.Model/Singerinfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSheet.Model
+{
+   public class Singerinfo
+    {
+        //歌手信息表
+        public int SingerId { get; set; }
+        public string SingerName { get; set; }
+        public string SingerClass { get; set; }
+        public string SingerRegion { get; set; }
+        public string HardImg { get; set; }
+        
+    }
+}
---
+++ Enterprisepersonalitydisplay/DataSheet.Model/UserCollect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSheet.Model
+{
+   public class UserCollect
+    {
+        //用户收藏表
+        public int CollectId { get; set; }
+        public int UserId { get; set; }
+        public int MicId { get; set; }
+
+    }
+}
