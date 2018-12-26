269ec9c6add1cd04a05c442d6600736f66e2a6bd
Fananly <1835676864@qq.com>
2018-12-26 13:12:11 +0800

    1123

--- Enterprisepersonalitydisplay/DataSheetDAL/MusicCRUD_F.cs
+++ Enterprisepersonalitydisplay/DataSheetDAL/MusicCRUD_F.cs
@@ -1,5 +1,6 @@
 ﻿using DataSheet.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -11,7 +12,7 @@ namespace DataSheetDAL
 {
     public class MusicCRUD_F
     {
-        public static List<MusicInfo> query(string condition)
+        public static List<ArrayList> query(string condition)
         {
             //音乐信息表对应的音乐信息类
            
@@ -21,20 +22,31 @@ namespace DataSheetDAL
                 new SqlParameter ("@MicName",condition),
                 new SqlParameter("@SingerName",condition)
             };
-            
-            string sql = "select * from MusicInfo m,SingerInfo s where m.SingerId=s.SingerId and m.MicName like '%@MicName%' or s.SingerName like '%@SingerName%'";
-            DataTable table = DBHelpe.SelectDB(sql, false, paras);
-            List<MusicInfo> tab = new List<MusicInfo>();
+
+
+            string sql = "select m.MicId,m.MicImg,m.MicName,m.MIcPlayCount,m.MicRegion,m.MicSignTime,m.MicSRc,ms.StyleName,s.SingerName   from MusicInfo m,SingerInfo s,MusicStyleInfo ms where m.SingerId = s.SingerId and m.StyleId = ms.StyleId and m.MicName like '%'+@MicName+'%' or s.SingerName like '%'+@SingerName+'%'";
+            DataTable table = DBHelpe.SelectDB(sql, false,paras);
+            List<ArrayList> list = new List<ArrayList>();
             foreach (DataRow item in table.Rows)
             {
-                tab.Add(new MusicInfo
-                {
-                    MicId = Convert.ToInt32(item["MicId"]),
-                    MicName = item["MicName"].ToString(),
-                });
+                list.Add(
+                    new ArrayList {
+                        item[0],
+                        item[1],
+                        item[2],
+                        item[3],
+                        item[4],
+                        item[5],
+                        item[6],
+                        item[7],
+                        item[8]
+                    }
+                    );
 
             }
-            return tab;
+
+
+            return list;
         }
 
     }
