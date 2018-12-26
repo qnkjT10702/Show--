b515040d3f0a9dcc95d7a9ba430b60ecc85ffaeb
lijiasowhat12321 <2064788658@qq.com>
2018-12-26 13:10:28 +0800

    控件

--- Enterprisepersonalitydisplay/Enterprise.UI/Admin/AdminLogin.aspx
+++ Enterprisepersonalitydisplay/Enterprise.UI/Admin/AdminLogin.aspx
@@ -1,5 +1,10 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdminLogin.aspx.cs" Inherits="Enterprise.UI.Admin.AdminLogin" %>
 
+
+
+
+
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -10,6 +15,8 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            
+           
         </div>
     </form>
 </body>
--- Enterprisepersonalitydisplay/Enterprise.UI/MusicMenu.ascx.cs
+++ Enterprisepersonalitydisplay/Enterprise.UI/MusicMenu.ascx.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataSheet.Model;
 
 namespace Enterprise.UI
 {
     public partial class MusicMenu : System.Web.UI.UserControl
     {
+        public MusicInfo info { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //Imagehead.ImageUrl = info.MicImg;
+            //rmusic.InnerText = info.MicName;
         }
     }
 }
