d6fd78c8b24156b5ea8c8c9647e43993dc33c248
LiaoxinJion <45959420+LiaoxinJion@users.noreply.github.com>
2019-01-07 14:56:40 +0800

    改善一些代码

--- Enterprisepersonalitydisplay/Enterprise.UI/HomePage.aspx.designer.cs
+++ Enterprisepersonalitydisplay/Enterprise.UI/HomePage.aspx.designer.cs
@@ -65,5 +65,32 @@ namespace Enterprise.UI {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Image2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+        
+        /// <summary>
+        /// Label3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
     }
 }
